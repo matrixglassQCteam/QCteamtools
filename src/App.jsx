5148b9fef446ec43a6b19fcde8af2d5f6aeb3eac
noobwebdev777 <webdev.proj.777@gmail.com>
2026-01-16 20:12:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+//     function rectangleDiagonal(length, width) {
+//   return Math.sqrt(length ** 2 + width ** 2);
+// }
+
+// // Example usage:
+// const diagonal = rectangleDiagonal(6, 8);
+// console.log(diagonal); // 10
+
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
